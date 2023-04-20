e23993490a7f2e0cc63987d345ad2f49fcfc80d1
Jrge-V <jorgeverduzco91@gmail.com>
2023-04-20 13:03:56 -0700

    added a way to find by name instead of button call

--- marvel.js
+++ marvel.js
@@ -1,39 +1,43 @@
 const publicKey = "da4d97f480cf88990079b8840f27623c";
 const privateKey = "f718261b4e6d51f28d90987960691bcd83c1e32b";
 
-
-const timestamp = new Date().getTime();
-
-const hash = md5(`${timestamp}${privateKey}${publicKey}`);
-
-
-const MARVEL_URL = 
-`https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${publicKey}&hash=${hash}`;
+const MARVEL_URL = (name) => {
+  const timestamp = new Date().getTime();
+  const hash = md5(`${timestamp}${privateKey}${publicKey}`);
+  return `https://gateway.marvel.com/v1/public/characters?name=${name}&ts=${timestamp}&apikey=${publicKey}&hash=${hash}`;
+};
 
 const marvel = document.querySelector(".characters");
 
-function addNewCharacter() {
-  const promise = fetch(MARVEL_URL);
-  promise 
-  .then(function(response) {
-    console.log("OG Response from API", response);
-    const processingPromise = response.json();
-    return processingPromise;
-  })
-  .then(function(processedResponse) {
-    console.log("Response after .json()", processedResponse);
-    const character = processedResponse.data.results[19];
-    const thumbnail = character.thumbnail;
-    const img = document.createElement("img");
+function addNewCharacter(name) {
+  const url = MARVEL_URL(name);
+  const promise = fetch(url);
+    promise
+      .then(function (response) {
+        console.log("OG Response from API", response);
+        const processingPromise = response.json();
+        return processingPromise;
+      })
+      .then(function (processedResponse) {
+        console.log("Response after .json()", processedResponse);
+        const character = processedResponse.data.results[0];
+        if (!character) {
+          console.log(`Character "${name}" not found.`);
+          return;
+        }
+        const thumbnail = character.thumbnail;
+        const img = document.createElement("img");
 
-    img.src = `${thumbnail.path}.${thumbnail.extension}`;
-    img.alt = character.name;
-    marvel.appendChild(img);
-  });
+        img.src = `${thumbnail.path}.${thumbnail.extension}`;
+        img.alt = character.name;
+        marvel.appendChild(img);
+      });
   console.log("this will log first");
 }
 
-document.querySelector(".add-character").addEventListener("click", addNewCharacter);
-
-
-
+const form = document.querySelector(".add-character-form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const name = form.querySelector(".character-name-input").value;
+  addNewCharacter(name);
+});
